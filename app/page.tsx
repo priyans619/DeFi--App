fbfe6b5241cf8571badcbceb337eb8a124e893bd
priyans619 <priyansh.2b@gmail.com>
2024-06-07 22:04:53 +0530

    Render Metamask

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Metamask from '@/components/Metamask';
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl mb-6">DeFi App</h1>
-      
+      <Metamask />
     </div>
   );
 };
