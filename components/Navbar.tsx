2370f118b64499844df50895e3d309fce8d2df70
priyans619 <priyansh.2b@gmail.com>
2024-06-07 23:24:52 +0530

    Add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="py-4 bg-gray-800 flex justify-between items-center px-8">
+      <h1 className="text-4xl text-white">DeFi App</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
