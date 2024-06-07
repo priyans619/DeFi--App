4f882d52ed5f6e49550221f71f82f47e733c274e
priyans619 <priyansh.2b@gmail.com>
2024-06-07 21:13:38 +0530

    button props

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 interface CustomButtonProps {
-  
+  btnType: 'button' | 'submit' | 'reset';
+  title: string;
+  handleClick: () => void;
+  styles?: string;
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({  }) => {
