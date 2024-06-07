11a10da4d125cadb7307e41c3664214e0a5e2ec6
priyans619 <priyansh.2b@gmail.com>
2024-06-07 23:25:21 +0530

    Render metamask

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Metamask from '@/components/Metamask';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="py-4 bg-gray-800 flex justify-between items-center px-8">
       <h1 className="text-4xl text-white">DeFi App</h1>
+      <Metamask />
     </nav>
   );
 };
