e38f9a211fbe7fb0478b308de3b7346f93a406bc
priyans619 <priyansh.2b@gmail.com>
2024-06-07 21:28:26 +0530

    add provider, web3, account for metamak

--- components/Metamask.tsx
+++ components/Metamask.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Web3Modal from 'web3modal';
+import Web3 from 'web3';
 
 
 const ConnectMetaMask: React.FC = () => {
-  
+  const [provider, setProvider] = useState<any>(null);
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [account, setAccount] = useState<string | null>(null);
+
+    const instance = await web3Modal.connect();
+    const web3 = new Web3(instance);
+    const accounts = await web3.eth.getAccounts();
+
+   
+  };
 
   return (
     
