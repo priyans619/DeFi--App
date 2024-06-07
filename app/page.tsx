3c70e88bcb4fbcfcb27deeab0ef6ac2e9970205a
priyans619 <priyansh.2b@gmail.com>
2024-06-07 23:26:04 +0530

    Update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Metamask from '@/components/Metamask';
+import Navbar from '@/components/Navbar';
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl mb-6">DeFi App</h1>
-      <Metamask />
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    
+      </div>
     </div>
   );
 };
