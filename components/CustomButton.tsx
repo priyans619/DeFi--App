0c0aad66b49696cb6be4ca801518f2a856d852aa
priyans619 <priyansh.2b@gmail.com>
2024-06-07 21:13:00 +0530

    Add custom button component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CustomButtonProps {
+  
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({  }) => {
+  return (
+    
+  );
+};
+
+export default CustomButton;
