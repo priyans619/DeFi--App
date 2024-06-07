239a4cd4184a75e90275377c4f44a99274119b57
priyans619 <priyansh.2b@gmail.com>
2024-06-07 21:31:21 +0530

    connect wallet functionality

--- components/Metamask.tsx
+++ components/Metamask.tsx
@@ -8,14 +8,28 @@ const ConnectMetaMask: React.FC = () => {
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [account, setAccount] = useState<string | null>(null);
 
+  const connectWallet = async () => {
+    const web3Modal = new Web3Modal({
+      cacheProvider: true,
+    });
     const instance = await web3Modal.connect();
     const web3 = new Web3(instance);
     const accounts = await web3.eth.getAccounts();
 
-   
+    setProvider(instance);
+    setWeb3(web3);
+    setAccount(accounts[0]);
+
+    instance.on('accountsChanged', (accounts: string[]) => {
+      setAccount(accounts[0]);
+    })
   };
 
   return (
+
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      
+    </div>
     
   );
 };
