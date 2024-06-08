c63a6a5e093afcfa194a0fe42d662053e9a60110
priyans619 <priyansh.2b@gmail.com>
2024-06-08 21:09:06 +0530

    Response data with datasets

--- components/CryptoPriceChart.tsx
+++ components/CryptoPriceChart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 import axios from 'axios';
 
@@ -25,6 +26,25 @@ const CryptoPriceChart: React.FC = () => {
           }
         );
 
+        const prices = response.data.prices;
+
+        setChartData({
+          labels: prices.map((price: any) =>
+            new Date(price[0]).toLocaleDateString()
+          ),
+          datasets: [
+            {
+              label: 'Bitcoin Price (USD)',
+              data: prices.map((price: any) => price[1]),
+              borderColor: '#4b6cb7',
+              backgroundColor: 'rgba(75, 108, 183, 0.2)',
+              pointBackgroundColor: '#4b6cb7',
+              pointBorderColor: '#fff',
+              pointHoverBackgroundColor: '#fff',
+              pointHoverBorderColor: '#4b6cb7',
+            },
+          ],
+        });
       } catch (error) {
         console.error('Error fetching price data', error);
       }
