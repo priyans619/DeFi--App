777e33e15678d592302b24d58103a209ba15a8c2
priyans619 <priyansh.2b@gmail.com>
2024-06-08 21:22:15 +0530

    import Chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
+import CryptoPriceChart from '@/components/CryptoPriceChart';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    
+      <div className="flex h-screen  flex-col items-center justify-center min-h-screen py-2">
+        <CryptoPriceChart />
       </div>
     </div>
   );
