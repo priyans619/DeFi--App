43cfa417312805c01ba8844a4cecd224994f9aaa
priyans619 <priyansh.2b@gmail.com>
2024-06-08 22:01:42 +0530

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,8 @@ import Metamask from '@/components/Metamask';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="py-4 bg-gray-800 flex justify-between items-center px-8">
-      <h1 className="text-4xl text-white">DeFi App</h1>
+    <nav className="py-2 bg-gray-800 flex justify-between items-center px-8">
+      <h1 className="text-3xl text-white">DeFi App</h1>
       <Metamask />
     </nav>
   );
