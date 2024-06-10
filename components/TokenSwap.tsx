d0e6681de5247d1f6e7d614d9a3556379d92c973
priyans619 <priyansh.2b@gmail.com>
2024-06-10 19:31:46 +0530

    Add tokenSwap comp

---
+++ components/TokenSwap.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+const TokenSwap: React.FC = () => {
+  
+  };
+
+  
+
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-lg">
+      <h2 className="text-xl font-semibold mb-4">Token Swap</h2>
+      
+    </div>
+  );
+};
+
+export default TokenSwap;
