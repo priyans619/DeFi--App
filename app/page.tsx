80553e1e9eecfafb2e636a7b4fce7c50bfe709af
priyans619 <priyansh.2b@gmail.com>
2024-06-10 19:37:09 +0530

    render TokenSwap

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import CryptoPriceChart from '@/components/CryptoPriceChart';
+import TokenSwap from '@/components/TokenSwap';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="relative min-h-screen bg-violet-100">
       <Navbar />
-      <div className="flex h-screen bg-violet-100  flex-col items-center justify-center min-h-screen py-2">
-        <CryptoPriceChart />
+      <div className="pt-16 flex flex-col items-center min-h-screen py-2">
+        <div className="flex flex-row w-full justify-center space-x-10">
+          <CryptoPriceChart />
+          <TokenSwap />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
+
