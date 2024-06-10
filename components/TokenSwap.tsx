9d50e5513b943ccdc0a7b393eaeb5daa5e531aff
priyans619 <priyansh.2b@gmail.com>
2024-06-10 19:32:34 +0530

    getQuote functionality for fetching

--- components/TokenSwap.tsx
+++ components/TokenSwap.tsx
@@ -4,7 +4,24 @@ import { ethers } from 'ethers';
 import axios from 'axios';
 
 const TokenSwap: React.FC = () => {
-  
+  const [fromToken, setFromToken] = useState('');
+  const [toToken, setToToken] = useState('');
+  const [amount, setAmount] = useState('');
+  const [quote, setQuote] = useState<any | null>(null);
+
+  const getQuote = async () => {
+    try {
+      const response = await axios.get(`https://api.0x.org/swap/v1/quote`, {
+        params: {
+          sellToken: fromToken,
+          buyToken: toToken,
+          sellAmount: ethers.utils.parseUnits(amount, 18).toString(),
+        },
+      });
+      setQuote(response.data);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
   };
 
   
